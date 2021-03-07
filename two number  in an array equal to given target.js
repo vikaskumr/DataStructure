f71339877b671ac69073283ab924f7e45c7b5a8b
Vikas Kumar <vikas.kumar@springworks.in>
2021-03-08 00:55:05 +0530

    Update two number  in an array equal to given target.js

--- two number  in an array equal to given target.js
+++ two number  in an array equal to given target.js
@@ -7,7 +7,9 @@ run a for loop and for every element calculate the difference between target and
 
 if(you find that difference in obj then console.log(obj[diff], i));
 else
-make an object to store the value */
+make an object to store the value *
+
+return an array of indices of elements whose sum forms the target number/
 
 let arr = [11, 15, 2, 7, 11];
 
